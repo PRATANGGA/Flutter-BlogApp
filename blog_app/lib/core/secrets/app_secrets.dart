326c008a6426f3df77dd26fb0d9ebe4a4d700fed
PRATANGGA <pratanggajulang31@gmail.com>
2024-08-12 00:54:22 +0700

    Initial commit

--- blog_app/lib/core/secrets/app_secrets.dart
+++ blog_app/lib/core/secrets/app_secrets.dart
@@ -1,5 +1,6 @@
 class AppSecrets {
-  static const supabaseUrl = 'https://yfqnooaidskgpplvwcpc.supabase.co';
+  static const supabaseUrl = 'https://jicfnwxyviaafoxfccen.supabase.co';
   static const supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlmcW5vb2FpZHNrZ3BwbHZ3Y3BjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU0NTM4NTYsImV4cCI6MjAzMTAyOTg1Nn0.rh02KxbqMIGHwHk6CoVCoWYozej-RVybQWW_fYGKopQ';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppY2Zud3h5dmlhYWZveGZjY2VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMzODkwOTgsImV4cCI6MjAzODk2NTA5OH0.FdZe-_Oakn0Oy49phfAprh5IhSbe4e72-kTWyItUbac';
 }
+//pratangga31
---
+++ blog_app/lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -0,0 +1,34 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract interface class AuthRemoteDataSource {
+  Future<String> signUpWithEmailAndPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+  Future<String> loginWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+}
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  final SupabaseClient supabaseClient;
+  AuthRemoteDataSourceImpl(this.supabaseClient);
+  @override
+  Future<String> loginWithEmailAndPassword(
+      {required String email, required String password}) {
+    // TODO: implement loginWithEmailAndPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> signUpWithEmailAndPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) {
+    // TODO: implement signUpWithEmailAndPassword
+    throw UnimplementedError();
+  }
+}
